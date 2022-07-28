39fc6ddce6e15f4537fb6ab43db03ad8ecb25a50
odev <ts8irolon@gmail.com>
2022-07-28 17:59:05 -0300

    Abstraccion del atributo Description

---
+++ src/Operators/domain/Identification.ts
@@ -0,0 +1,19 @@
+export class Identification {
+  private id: string;
+
+  public constructor(id: string) {
+    this.checkLong(id);
+
+    this.id = id;
+  }
+
+  public checkLong(id: string) {
+    if (id.length !== 8) {
+      throw new Error(`Identification require 8 digits`);
+    }
+  }
+
+  public value() {
+    return this.id;
+  }
+}
