47ddac093ec35482d372d4ab2913675a7d924832
odev <ts8irolon@gmail.com>
2022-08-01 22:28:03 -0300

    Añado validacion con expresion regular. <Operator#2020>

--- src/Operators/domain/Credentials.ts
+++ src/Operators/domain/Credentials.ts
@@ -2,7 +2,14 @@ export class Credentials {
   private discordCompleteId: string;
 
   public constructor(discordCompleteId: string) {
+    this.isDiscordCompleteId(discordCompleteId);
     this.discordCompleteId = discordCompleteId;
+  }
+
+  private isDiscordCompleteId(discordCompleteId: string) {
+    if (!/[#]..../.test(discordCompleteId)) {
+      throw new Error('Discord name is invalid. Example: operator#2032');
+    }
   }
 
   public value() {
