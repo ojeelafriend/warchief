08fc65d6895ccc103dda416c089197bb34f209dd
odev <ts8irolon@gmail.com>
2022-08-01 22:30:37 -0300

    Añado test a validaciones y getValue de Operator

---
+++ test/Operators/domain/Operator.test.ts
@@ -0,0 +1,37 @@
+import { Operator } from '../../../src/Operators/domain/Operator';
+
+describe('Operator creational entity: ', () => {
+  const params = {
+    bad: {
+      credentials: 'badparams',
+      id: '20ks',
+      fullName: 'rev',
+    },
+    ok: {
+      credentials: 'okparams#2020',
+      id: '204034ks',
+      fullName: 'Bjorn F. Bronnfjell',
+
+      rank: 1,
+      operations: 0,
+      status: false,
+      score: 10,
+    },
+  };
+
+  test('should not create operator bad params', () => {
+    const { credentials, id, fullName } = params.bad;
+
+    expect(() => {
+      Operator.create(credentials, id, fullName);
+    }).toThrow();
+  });
+
+  test('should create operator ok params', () => {
+    const { credentials, id, fullName } = params.ok;
+
+    const operator = Operator.create(credentials, id, fullName);
+
+    expect(operator.details()).toStrictEqual(params.ok);
+  });
+});
