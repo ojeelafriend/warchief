346d453ed2d88b7ec835549f71e1bb04b38aa1d4
odev <ts8irolon@gmail.com>
2022-08-18 09:00:34 -0300

    Replace: Network for Api

--- src/Network/http/express.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import express from 'express';
-
-export const getApi = (): Express => {
-  const api = express();
-  api.use(express.json(), express.urlencoded({ extended: false }));
-
-  return api;
-};
--- src/Network/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { Express } from 'express';
-import operator from './operator';
-
-export const routesManager = (server: Express) => {
-  server.use('/operator', operator);
-};
