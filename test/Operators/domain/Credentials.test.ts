c91fe1045dfafb0a7e82b320b6ec1781ad623a5c
odev <ts8irolon@gmail.com>
2022-08-18 09:19:06 -0300

    Test: añado exceeded para validar si se excede e.g operator#2222222

--- test/Operators/domain/Credentials.test.ts
+++ test/Operators/domain/Credentials.test.ts
@@ -4,6 +4,7 @@ describe('Credentials creational entity: ', () => {
   const wrapper = {
     incompleteId: 'operator#23',
     completeId: 'operator#3702',
+    exceeded: 'operator#37022222',
   };
 
   function createCredentials(discordId: string) {
@@ -14,6 +15,12 @@ describe('Credentials creational entity: ', () => {
   test('should not create entity discordId incomplete', () => {
     expect(() => {
       createCredentials(wrapper.incompleteId);
+    }).toThrow();
+  });
+
+  test('should not create entity discordId exceeded', () => {
+    expect(() => {
+      createCredentials(wrapper.exceeded);
     }).toThrow();
   });
 
