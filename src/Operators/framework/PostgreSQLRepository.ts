45bfb21df465c5bab4e3cbc28c287cddc71666b4
odev <ts8irolon@gmail.com>
2022-08-23 10:56:01 -0300

    Implemento ListById y ListAll con sus queries

--- src/Operators/framework/PostgreSQLRepository.ts
+++ src/Operators/framework/PostgreSQLRepository.ts
@@ -1,6 +1,8 @@
-import { repository } from '../../Api/server';
 import { IRepository } from '../domain/IRepository';
+import { Credentials } from '../domain/Credentials';
 import { Operator } from '../domain/Operator';
+
+import { repository } from '../../Api/server';
 
 export class PostgreSQLRepository implements IRepository {
   async save(operator: Operator): Promise<void> {
@@ -13,5 +15,29 @@ export class PostgreSQLRepository implements IRepository {
     params = [credentials, identification, fullName, rank, status, score, operations];
 
     await repository.query(sql, params);
+  }
+
+  async listById(credentials: Credentials): Promise<any[]> {
+    let params: string[];
+
+    params = [credentials.value()];
+
+    const sql: string = `SELECT * FROM operator WHERE credentials=$1`;
+
+    const info = await repository.query(sql, params);
+
+    return info.rows;
+  }
+
+  async listAll(initial: number): Promise<any[]> {
+    let params: number[];
+
+    params = [initial, 2];
+
+    const sql: string = `SELECT * FROM operator OFFSET $1 LIMIT $2`;
+
+    const { rows } = await repository.query(sql, params);
+
+    return rows;
   }
 }
