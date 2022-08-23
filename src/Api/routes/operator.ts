ade8671d67e4256ce7a05fd3c467eac426fe2d79
odev <ts8irolon@gmail.com>
2022-08-23 10:57:30 -0300

    Creo dos rutas dinamicas que traerán los operadores: por uno o todos

--- src/Api/routes/operator.ts
+++ src/Api/routes/operator.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response } from 'express';
 
-import { Creator } from '../../Operators/application/Creator';
 import { PostgreSQLRepository } from '../../Operators/framework/PostgreSQLRepository';
 
-const repository = new PostgreSQLRepository();
-const creator = new Creator(repository);
+import { Creator } from '../../Operators/application/Creator';
+import { ListOne } from '../../Operators/application/ListOne';
+import { List } from '../../Operators/application/List';
 
 const router: Router = Router();
+
+const repository = new PostgreSQLRepository();
+
+const creator = new Creator(repository);
+const listOne = new ListOne(repository);
+const list = new List(repository);
 
 router.post('/', async (req: Request, res: Response) => {
   const { credentials, identification, fullName } = req.body;
@@ -20,9 +26,27 @@ router.post('/', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/:credentials', (req: Request, res: Response) => {
+router.get('/:credentials', async (req: Request, res: Response) => {
   const { credentials } = req.params;
-  res.json(credentials);
+
+  try {
+    let operator = await listOne.run(credentials.replace('$', '#'));
+
+    res.json(operator).status(200);
+  } catch (err) {
+    res.json({ failed: err }).status(404);
+  }
+});
+
+router.get('/', async (req: Request, res: Response) => {
+  const { page } = req.query;
+  try {
+    const operators = await list.run(page);
+
+    res.json(operators).status(200);
+  } catch (err) {
+    res.json({ failed: `Internal Error` }).status(500);
+  }
 });
 
 export default router;
